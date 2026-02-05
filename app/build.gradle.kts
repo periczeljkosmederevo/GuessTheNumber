4b18dbc34c095890c984502686305b361c85b7ec
Željko <periczeljkosmederevo@yahoo.com>
2026-02-05 13:12:54 +0100

    updated manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,13 @@
 import com.android.build.api.dsl.ApplicationExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
 
-extensions.configure<ApplicationExtension>("android") {
+configure<ApplicationExtension> {
     namespace = "com.periczeljkosmederevo.myapplication"
     compileSdk = 36
 
@@ -14,20 +15,24 @@ extensions.configure<ApplicationExtension>("android") {
         applicationId = "com.periczeljkosmederevo.myapplication"
         minSdk = 23
         targetSdk = 36
-        versionCode = 16
-        versionName = "16.0"
+        versionCode = 17
+        versionName = "17.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        ndk {
+            abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64"))
+        }
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
@@ -36,15 +41,15 @@ extensions.configure<ApplicationExtension>("android") {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
+    @Suppress("UnstableApiUsage")
     bundle {
-        language {
-            @Suppress("UnstableApiUsage")
-            enableSplit = false
-        }
+        language { enableSplit = false }
+        density { enableSplit = false }
+        abi { enableSplit = true }
     }
 }
 
-kotlin {
+tasks.withType<KotlinCompile>().configureEach {
     compilerOptions {
         jvmTarget.set(JvmTarget.JVM_11)
     }
